15e8f6710e71e7e6b2f6171ef0c3356da3bd723d
rockycsumb <romoreno@csumb.edu>
2020-08-26 06:28:13 +0000

    added sidebar and icons

---
+++ coderchat-app/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyjDbIzQNIoiFmHtp1E-0LIWJ56oWghEE",
+  authDomain: "coderchat-c0a8c.firebaseapp.com",
+  databaseURL: "https://coderchat-c0a8c.firebaseio.com",
+  projectId: "coderchat-c0a8c",
+  storageBucket: "coderchat-c0a8c.appspot.com",
+  messagingSenderId: "397689870574",
+  appId: "1:397689870574:web:d16f6de28f8ab82c187bff",
+  measurementId: "G-6YD017ZJ3H"
+};
